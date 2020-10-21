b673d20e790f9f0c3e72c589982d046c640b1235
Heng-Chuan Lin <z5219960@student.unsw.edu.au>
2020-10-22 00:55:20 +1100

    basic communication between controller and router

--- controller.erl
+++ controller.erl
@@ -1,13 +1,35 @@
-%%%-------------------------------------------------------------------
-%%% @author tommy
-%%% @copyright (C) 2020, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 22. Oct. 2020 12:00 am
-%%%-------------------------------------------------------------------
 -module(controller).
+-import(router,[start/1]).
 -author("tommy").
 
 %% API
--export([]).
+-export([test/0,genGraph/0,graphToNetwork/1]).
+test()->
+  G = genGraph(),
+  graphToNetwork(G).
+genGraph() ->
+  [{red  , [{white, [white, green]},
+    {blue , [blue]}]},
+    {white, [{red, [blue]},
+      {blue, [green, red]}]},
+    {blue , [{green, [white, green, red]}]},
+    {green, [{red, [red, blue, white]}]}
+  ].
+graphToNetwork(Graph) ->
+  io:format("graph is ~w~n",[Graph]),
+  nodeSpawn(Graph).
+
+edgeIterate([])->ok;
+edgeIterate([First|Rest])->
+  {Edge,Names} = First,
+  io:format("Edge is ~w~n",[Edge]).
+
+nodeSpawn([]) -> ok;
+nodeSpawn([First|Rest]) ->
+  {NodeName,Edges} = First,
+  io:format("Node is ~w~n",[NodeName]),
+  NodePid = router:start(NodeName),
+  edgeIterate(Edges),
+  ControlFun = hello_world,
+  NodePid ! {control, self(), self(), 0,ControlFun},
+  nodeSpawn(Rest).
--- router.erl
+++ router.erl
@@ -1,13 +1,18 @@
-%%%-------------------------------------------------------------------
-%%% @author tommy
-%%% @copyright (C) 2020, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 21. Oct. 2020 11:59 pm
-%%%-------------------------------------------------------------------
 -module(router).
--author("tommy").
+-export([start/1]).
 
-%% API
--export([]).
+
+start(RouterName) ->
+  spawn(fun()-> process(RouterName) end).
+
+process(RouterName)->
+  receive
+    {control, From, Pid, 0,Content} ->
+      io:format("~w recv ~w from ~w~n",[RouterName,Content,Pid]),
+      process(RouterName);
+    {control, From, Pid, SeqNum,Content} ->
+      io:format("I recv not 0 seq from ~n"),
+      process(RouterName)
+
+  end,
+  ok.
