00580234c356db60c94af8795b6bf6e25777abf9
Heng-Chuan Lin <z5219960@student.unsw.edu.au>
2020-10-30 01:40:52 +1100

    timeout

--- router.erl
+++ router.erl
@@ -6,12 +6,12 @@
 
 start(RouterName) ->
   Table = ets:new(routing_table,[public]),
-  SpawnPid = spawn(fun()-> process(RouterName,Table) end),
+  SpawnPid = spawn(fun()-> process(RouterName,Table,0) end),
   SpawnPid.
   
 
 
-process(RouterName,Table)->
+process(RouterName,Table,Cur_SeqNum)->
   receive
     % DestNode receive the message
     % {message, Dest, From,Pid,Trace} when Dest == RouterName ->
@@ -27,39 +27,47 @@ process(RouterName,Table)->
       ControlFun(RouterName,Table),
       Obj = ets:match_object(Table,{'$0','$1'}),
       % io:format("[0] Routing Talbe of ~w (pid: ~w) : ~p~n",[RouterName,self(),Obj]),
-      process(RouterName,Table);
+      process(RouterName,Table,SeqNum);
+    
     {control, From, Pid, SeqNum, ControlFun} when From == Pid ->
       % I am a root router
-      ForwardMessage = {control, From, Pid, SeqNum, ControlFun},
+      % ask all nodes can commit or not
       ListofEntries = ets:match_object(Table,{'$0','$1'}),
-      _Children = ControlFun(RouterName,Table),
+      send_msg_to_all_nodes(ListofEntries,Pid,RouterName),
+      receive
+      
+      end,
+      % io:format("Root Node~w : Pid: ~w From: ~w~n",[RouterName,Pid,From]),
+      ForwardMessage = {control, self(), Pid, SeqNum, ControlFun},
+      
+      Children = ControlFun(RouterName,Table),
+      io:format("Children is ~w~n",[Children]),
       io:format("list of entry from ~w : ~p~n",[RouterName,ListofEntries]),
-      % propagate_control_message(ListofEntries,[],ForwardMessage),
+      propagate_control_message(ListofEntries,[],ForwardMessage),
       Obj = ets:match_object(Table,{'$0','$1'}),
       io:format("[~w] Routing Talbe of ~w (pid: ~w) : ~p~n",[SeqNum,RouterName,self(),Obj]),
       % eventually send to controller
       % Pid ! {committed,self(),SeqNum},
       % % or
       % Pid ! {abort,self(),SeqNum},
-      process(RouterName,Table);
-    {control, From, _Pid, SeqNum, ControlFun} when From == RouterName -> 
-      ControlFun(RouterName,Table),
-      Obj = ets:match_object(Table,{'$0','$1'}),
-      io:format("[~w] Routing Talbe of ~w (pid: ~w) : ~p~n",[SeqNum,RouterName,self(),Obj]),
-      process(RouterName,Table);
+      process(RouterName,Table,SeqNum);
     {control, From, Pid, SeqNum, ControlFun} -> 
-      io:format("~w receive the propagate control msg~n",[RouterName]),
-      ForwardMessage = {control, From, Pid, SeqNum, ControlFun},
-      ListofEntries = ets:match_object(Table,{'$0','$1'}),
-      _Children = ControlFun(RouterName,Table),
-      propagate_control_message(ListofEntries,[],ForwardMessage),
-      Obj = ets:match_object(Table,{'$0','$1'}),
-      io:format("[~w] Routing Talbe of ~w (pid: ~w) : ~p~n",[SeqNum,RouterName,self(),Obj]),
-      process(RouterName,Table);
+      if
+        Cur_SeqNum < SeqNum ->
+          io:format("~w receive the propagate control msg~n",[RouterName]),
+          ForwardMessage = {control, From, Pid, SeqNum, ControlFun},
+          ListofEntries = ets:match_object(Table,{'$0','$1'}),
+          _Children = ControlFun(RouterName,Table),
+          propagate_control_message(ListofEntries,[],ForwardMessage),
+          Obj = ets:match_object(Table,{'$0','$1'}),
+          io:format("[~w] Routing Talbe of ~w (pid: ~w) : ~p~n",[SeqNum,RouterName,self(),Obj]),
+          process(RouterName,Table,SeqNum);
+        true -> process(RouterName,Table,Cur_SeqNum)
+      end;
     {dump,From} ->
       Dump = ets:match(Table,'$1'),
       From ! {table,self(),Dump},
-      process(RouterName,Table);
+      process(RouterName,Table,Cur_SeqNum);
     stop ->
       % de-allocate the table
       ets:delete(Table),
@@ -70,6 +78,21 @@ process(RouterName,Table)->
 
   end.
 
+ask_nodes_for_commit([],ControlPid,RouterName) -> true;
+ask_nodes_for_commit([FirstEntry|RestEntries],ControlPid,RouterName) ->
+  {DestNodeName,RouteViaPid} = FirstEntry,
+  if
+    DestNodeName =/= '$NoInEdges' ->
+      RouteViaPid ! {can_you_commit,DestNodeName,self()};
+    true -> ok
+  end,
+  receive
+    {i_can_commit,SendToMe,From} -> yes;
+    {i_cannot_commitm,SendToMe,From} -> no
+  after 5000 ->
+            timeout
+  end,
+  ask_nodes_for_commit(RestEntries,ControlPid,RouterName).
 
 propagate_control_message([],Final_List,ForwardMessage) -> 
   io:format("forward node have ~p~n",[Final_List]),
@@ -77,13 +100,17 @@ propagate_control_message([],Final_List,ForwardMessage) ->
     RoutePid ! ForwardMessage
     end,Final_List);
 propagate_control_message([FirstEntry|RestEntries],List,ForwardMessage) ->
-  {_DestNodeName,RouteViaPid} = FirstEntry,
-  Result = lists:member(RouteViaPid, List),
+  {DestNodeName,RouteViaPid} = FirstEntry,
   if
-    Result == false ->
-      New_List = lists:append([RouteViaPid],List);
-    true ->
-      New_List = List
+    DestNodeName =/= '$NoInEdges' ->
+      Result = lists:member(RouteViaPid, List),
+      if
+        Result == false ->
+          New_List = lists:append([RouteViaPid],List);
+        true ->
+          New_List = List
+      end;
+    true -> New_List = List
   end,
   propagate_control_message(RestEntries,New_List,ForwardMessage).
 
