3993a8093511b8d43b229fe29f4260f15afb36b3
aks60808 <aks60808@gmail.com>
2020-11-03 01:07:16 +1100

    well-commented router.erl

--- router.erl
+++ router.erl
@@ -66,7 +66,7 @@ process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResu
           when DestNodeName == RouterName ->
           % I am the DestNode
           % check the tentative result
-          {Children, _TempTable} = TentativeResult,
+          {Children, _TempTable,_PreviousSeqNum} = TentativeResult,
           % record the trace from root to me
           [LastPid | Rest] = Trace,
           RoutingPath_from_Root_to_Node = [self()] ++ Trace ,
@@ -147,40 +147,52 @@ process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResu
                  ets:delete(TempTable),
                  % send abort to controller
                  Pid ! {abort, self(), SeqNum},
-                 % terminate the 2PC
+                 % complete the 2PC
                  process(RouterName,RouterName, Table, Cur_SeqNum, false, MsgQueue, {});
              true ->
+                 % send DoCommit to all node
                  send_2nd_phase_msg(ListofEntries, SeqNum, RouterName, doCommit,RoutingPath_Table),
+                 % get all Obj from Temp Routing table
                  AllObj_from_tempTable = ets:match_object(TempTable, {'$0', '$1'}),
+                 % replace the Routing Table 
                  ets:insert(Table, AllObj_from_tempTable),
+                 % de-allocate the Temp Table
                  ets:delete(TempTable),
                  % eventually send to controller
                  Pid ! {committed, self(), SeqNum},
                  % finished 2PC
-                 AllObj_from_table_now = ets:match_object(Table, {'$0', '$1'}),
-                %  io:format("~p~n", [AllObj_from_table_now]),
                  process(RouterName,RouterName, Table, SeqNum, false, MsgQueue, {})
           end;
       {control, From, Pid, SeqNum, ControlFun} ->
-          io:format("~w got propagate cntrl msg~n",[RouterName]),
+        % RootNode will receive outdated(last 2PC) propagated control msgs after 2PC
+        % Other Node will receive current propagated control msg more than once during 2PC
+        % In my implementation, RootNode will process this control msg if the last control Msg is aborted after 2PC
+        % so thats why I have the statement check wether the RootNode mathching the NdoeName
+
           if Cur_SeqNum < SeqNum andalso RootNode=/=RouterName->
                  ControlMessage = {control, From, Pid, SeqNum, ControlFun},
                  ListofEntries = ets:match_object(Table, {'$0', '$1'}),
                  propagate_control_message(ListofEntries, [], ControlMessage),
+                 % create temp routing table
                  TempTable = ets:new(temp_routing_table, [public]),
                  AllObj = ets:match_object(Table, {'$0', '$1'}),
+                 % copy all objs to temp routing table from current routing table
                  ets:insert(TempTable, AllObj),
+                 % perform the control msg
                  Children = ControlFun(RouterName, TempTable),
-                 Updated_TentativeResult = {Children, TempTable},
+                 % store the result, Temp routing and cur_seqNum 
+                 Updated_TentativeResult = {Children, TempTable,Cur_SeqNum},
+                 % set current SeqNum to this SeqNum indicating I'm performing SeqNum : X 2PC
                  process(RootNode,RouterName, Table, SeqNum, true, MsgQueue, Updated_TentativeResult);
              % old control message
              true ->
                  process(RootNode,RouterName, Table, Cur_SeqNum, false, MsgQueue, TentativeResult)
           end;
-      {doAbort, _SeqNum, DestNodeName, _FromPid, RootNodeName, Trace}
+      {doAbort, _SeqNum, DestNodeName, _DeliverPid, RootNodeName, Trace}
           when DestNodeName == RouterName ->
-          {Children, TempTable} = TentativeResult,
+          {Children, TempTable, PreviousSeqNum} = TentativeResult,
           if is_list(Children) == true ->
+                 % terminate spawned node if this node has it
                  lists:foreach(fun (NodePid) ->
                                        exit(NodePid, abort),
                                        io:format("NewSpawnedNode ~w alive? ~w~n",[NodePid,is_process_alive(NodePid)])
@@ -189,46 +201,51 @@ process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResu
              true -> % this is == abort
                  ok
           end,
+          % deallocate the temp routing table
           ets:delete(TempTable),
           [LastPid | Rest] = Trace,
+          % send abort ack back to Root
           LastPid ! {doAbort, ack, RootNodeName, Rest},
-          Obj_after_abort = ets:match_object(Table, {'$0', '$1'}),
-        %   io:format("~w after abort is ~p~n", [RouterName,Obj_after_abort]),
-          % revert to the previous status, close the 2PC
-          process(RootNode,RouterName, Table, Cur_SeqNum, false, MsgQueue, {});
-      {doAbort, SeqNum, DestNodeName, _FromPid, RootNodeName, Trace} ->
+          % restore to the previous status, close the 2PC
+          process(RootNode,RouterName, Table, PreviousSeqNum, false, MsgQueue, {});
+      {doAbort, SeqNum, DestNodeName, _DeliverPid, RootNodeName, Trace} ->
           % forwarding
           New_Trace = [self()] ++ Trace,
           [{_DestNodeName, RouterPid}] = ets:lookup(Table, DestNodeName),
           RouterPid ! {doAbort, SeqNum, DestNodeName, self(), RootNodeName, New_Trace},
-          io:format("~w ~w foward doAbort to ~w via ~w~n",
-                    [RouterName, self(), DestNodeName, RouterPid]),
           process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResult);
-      {doCommit, SeqNum, DestNodeName, FromPid, RootNodeName,_Deliver_List,Path_from_Root_to_Node}
+      {doCommit, SeqNum, DestNodeName, _DeliverPid, RootNodeName,_Deliver_List,Path_from_Root_to_Node}
           when DestNodeName == RouterName ->
-          io:format("~w ~w recv DoCommit from ~w~n", [RouterName, self(), FromPid]),
-          {_Children, TempTable} = TentativeResult,
+          % doCommit is different from doAbort
+          % my implementation is slightly different from normal 2PC
+          % I want to RootNode to know if the doCommit is actually perform
+          % However this would make some routing table of nodes a bit inconsistent
+          % some node will have already committed and reconfigured its routing table some won't
+          % Hence, I cacahed the Path based on current routing table in order to follow the 2PC flow from Spec.
+          {_Children, TempTable,_PreviousSeqNum} = TentativeResult,
           Path_from_Node_to_Root = lists:reverse(Path_from_Root_to_Node),
           [_MyPid | Rest] = Path_from_Node_to_Root,
           [NextPid|OtherPids] = Rest,
           NextPid ! {doCommit, ack, RootNodeName, OtherPids},
           AllObj_from_tempTable = ets:match_object(TempTable, {'$0', '$1'}),
+          % replace the current routing table with temp one
           ets:insert(Table, AllObj_from_tempTable),
+          % deallocate the temp table
           ets:delete(TempTable),
           % update the latest SeqNum, make TempTable permanent and close the 2PC
           process(RootNode,RouterName, Table, SeqNum, false, MsgQueue, {});
-      {doCommit, SeqNum, DestNodeName, _FromPid, RootNodeName, Deliver_List,RoutingPath} ->
+      {doCommit, SeqNum, DestNodeName, _DeliverPid, RootNodeName, Deliver_List,RoutingPath} ->
           % forwarding
           [NextPid|RestPids] = Deliver_List,
           NextPid ! {doCommit, SeqNum, DestNodeName, self(), RootNodeName, RestPids,RoutingPath},
           process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResult);
       {doCommit, ack, RootNodeName, Deliver_List} ->
+           % forwarding
           [LastPid | Rest] = Deliver_List,
           LastPid ! {doCommit, ack, RootNodeName, Rest},
-          io:format("~w ~w recv the doCommit ACK to ~w via ~w~n",
-                    [RouterName, self(), RootNodeName, LastPid]),
           process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResult);
       {doAbort, ack, RootNodeName, Trace} ->
+           % forwarding
           [LastPid | Rest] = Trace,
           LastPid ! {doAbort, ack, RootNodeName, Rest},
           process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResult);
@@ -237,7 +254,6 @@ process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResu
           From ! {table, self(), Dump},
           process(RootNode,RouterName, Table, Cur_SeqNum, false, MsgQueue, TentativeResult);
       stop ->
-        io:format("~w ~w recv stop!~n",[RouterName,self()]),
         Message = stop,
         if IsIn2PC == true -> % if I am in 2PC, queue this msg
                New_MsgQueue = MsgQueue ++ [Message],
@@ -245,30 +261,38 @@ process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResu
            true -> % if I am not in 2PC , operate this stop command
             % de-allocate the table    
             ets:delete(Table),
-            io:format("~w ~w leave ~n",[RouterName,self()]),
             exit(stop)
-        end
+        end;
+      _Unkown_msg ->
+        % This is the prevention of mailbox overflow -> keep consuming other message
+        process(RootNode,RouterName, Table, Cur_SeqNum, IsIn2PC, MsgQueue, TentativeResult)   
     end.
 
 send_2nd_phase_msg([], _SeqNum, _RouterName, _Command,_RoutingPath_Table) ->
     ok;
 send_2nd_phase_msg([FirstEntry | RestEntries], SeqNum, RouterName, Command,RoutingPath_Table) ->
     {DestNodeName, RouteViaPid} = FirstEntry,
+    % skip NoInEdge key
     if DestNodeName =/= '$NoInEdges' ->
-           io:format("send 2nd phase to ~w~n", [DestNodeName]),
            if Command == doCommit->
+            % get the path from path table
             [{_DestNodeName,Path_from_Root_to_Node}] = ets:lookup(RoutingPath_Table,DestNodeName),
             [_RootNodePid|Rest] = Path_from_Root_to_Node,
             [NextPid|OtherPids] = Rest,
+            % send doCommit
             NextPid ! {Command, SeqNum, DestNodeName, self(), RouterName, OtherPids,Path_from_Root_to_Node};
            true ->
+            % send doAbort
             RouteViaPid ! {Command, SeqNum, DestNodeName, self(), RouterName, [self()]}
            end,
            receive
              {Command, ack, _Me, _Trace} ->
-                 io:format("Root recv ack from ~n"),
                 ok
+            % if timeout then 2PC assumption has been broken
+            after 5000 ->
+                erlang:error("assumption of 2PC Break!!") 
            end,
+           % check the next entry -> make sure it's no key: NoInedges
            [NextEntry | OtherEntries] = RestEntries,
            {NextDestNodeName, _NextRouteViaPid} = NextEntry,
            if NextDestNodeName == '$NoInEdges' ->
@@ -284,13 +308,15 @@ ask_nodes_for_commit([], _RouterName,_RoutePATH_TABLE) ->
     true;
 ask_nodes_for_commit([FirstEntry | RestEntries], RouterName,RoutePATH_TABLE) ->
     {DestNodeName, RouteViaPid} = FirstEntry,
+    % skip NoInEdge key
     if DestNodeName =/= '$NoInEdges' ->
-        %    io:format("send cancommit to ~w~n", [DestNodeName]),
+           % ask node can you commit
            RouteViaPid ! {can_you_commit, DestNodeName, self(), RouterName, [self()]},
            receive
+             % node reply can Commit will attach their Path for further usage
              {i_can_commit, _Me, _RouteFromPid, FromNodeName, _Deliver_List,Path_from_Node_to_Root}
                  when FromNodeName == DestNodeName ->
-                 %  io:format("~w recv the canCommit from ~w~n", [RouterName, FromNodeName]),
+                 % reverse the order from delivery
                  Path_from_Root_to_Node = lists:reverse(Path_from_Node_to_Root),
                  ets:insert(RoutePATH_TABLE,{FromNodeName,Path_from_Root_to_Node}),
                  [NextEntry | OtherEntries] = RestEntries,
@@ -300,6 +326,7 @@ ask_nodes_for_commit([FirstEntry | RestEntries], RouterName,RoutePATH_TABLE) ->
                     true ->
                         ask_nodes_for_commit(RestEntries, RouterName,RoutePATH_TABLE)
                  end;
+             % if root recv this -> abort 
              {i_cannot_commit, _Me, _FromPid, _FromNodeName, _Trace} ->
                  abort
              after 5000 ->
@@ -310,7 +337,7 @@ ask_nodes_for_commit([FirstEntry | RestEntries], RouterName,RoutePATH_TABLE) ->
     end.
 
 propagate_control_message([], Final_List, ForwardMessage) ->
-    % io:format("forward node have ~p~n",[Final_List]),
+    % sending control msg
     lists:foreach(fun (RoutePid) ->
                           RoutePid ! ForwardMessage
                   end,
@@ -318,6 +345,7 @@ propagate_control_message([], Final_List, ForwardMessage) ->
 propagate_control_message([FirstEntry | RestEntries], List, ForwardMessage) ->
     {DestNodeName, RouteViaPid} = FirstEntry,
     if DestNodeName =/= '$NoInEdges' ->
+           % see this pid is in the outgoing list
            Result = lists:member(RouteViaPid, List),
            if Result == false ->
                   New_List = [RouteViaPid] ++ List;
